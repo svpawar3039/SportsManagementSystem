44a6fde7ec08340a63ff16de6885737cc237adbf
Gauravdond144 <92206175+Gauravdond144@users.noreply.github.com>
2023-08-31 15:53:30 -0400

    Add files via upload

---
+++ SportsManagement/src/Sports/Cricket.java
@@ -0,0 +1,65 @@
+package Sports;
+
+import java.util.Scanner;
+
+public class Cricket extends UserInput {
+	Scanner sc = new Scanner(System.in);
+	public void addPlayer() {
+		int baller = 4;
+		int batsman = 4;
+		int keeper = 1;
+		int allRounder = 2;
+		
+		System.out.println("Please Enter Your Name : ");
+		UserName = sc.next();
+		
+		System.out.println("Which Role Do You Want in Team");
+		UserRole = sc.next();
+		
+		while(isRoleAssigned != true) {
+			if(UserRole.equalsIgnoreCase("baller")) {
+				if(baller != 0) {
+					System.out.println(UserName + " assigned a Role of " + UserRole);
+					baller --;
+					isRoleAssigned = true;
+				}else {
+					System.out.println("This Role is No Longer Available");
+				}
+			}else if(UserRole.equalsIgnoreCase("batsman")) {
+				if(batsman != 0) {
+					System.out.println(UserName + " assigned a Role of " + UserRole);
+					batsman --;
+					isRoleAssigned = true;
+				}else {
+					System.out.println("This Role is No Longer Available");
+				}
+			}else if(UserRole.equalsIgnoreCase("wicket keeper") || UserRole.equalsIgnoreCase("keeper")) {
+				if(keeper != 0) {
+					System.out.println(UserName + " assigned a Role of " + UserRole);
+					keeper --;
+					isRoleAssigned = true;
+				}else {
+					System.out.println("This Role is No Longer Available");
+				}
+			}else if(UserRole.equalsIgnoreCase("all rounder")) {
+				if(allRounder != 0) {
+					System.out.println(UserName + " assigned a Role of " + UserRole);
+					baller --;
+					batsman --;
+					keeper --;
+					allRounder--;
+					isRoleAssigned = true;
+				}else {
+					System.out.println("This Role is No Longer Available");
+				}
+			}else {
+				System.out.println("Please Enter Valid Role");
+				sc.next();
+			}
+		}
+		
+	}
+	
+
+
+}
---
+++ SportsManagement/src/Sports/Football.java
@@ -0,0 +1,61 @@
+package Sports;
+
+import java.util.Scanner;
+
+public class Football extends UserInput {
+	Scanner sc = new Scanner(System.in);
+	public void addPlayer() {
+		int defender = 4;
+		int midfilder = 4;
+		int goalkeeper = 1;
+		int forward = 2;
+		
+		System.out.println("Please Enter Your Name : ");
+		UserName = sc.next();
+		
+		System.out.println("Which Role Do You Want in Team");
+		UserRole = sc.next();
+		
+		while(isRoleAssigned != true) {
+			if(UserRole.equalsIgnoreCase("defender")) {
+				if(defender != 0) {
+					System.out.println(UserName + " assigned a Role of " + UserRole);
+					defender --;
+					isRoleAssigned = true;
+				}else {
+					System.out.println("This Role is No Longer Available");
+				}
+			}else if(UserRole.equalsIgnoreCase("midfilder")) {
+				if(midfilder != 0) {
+					System.out.println(UserName + " assigned a Role of " + UserRole);
+					midfilder --;
+					isRoleAssigned = true;
+				}else {
+					System.out.println("This Role is No Longer Available");
+				}
+			}else if(UserRole.equalsIgnoreCase("goalkeeper") || UserRole.equalsIgnoreCase("keeper")) {
+				if(goalkeeper != 0) {
+					System.out.println(UserName + " assigned a Role of " + UserRole);
+					goalkeeper --;
+					isRoleAssigned = true;
+				}else {
+					System.out.println("This Role is No Longer Available");
+				}
+			}else if(UserRole.equalsIgnoreCase("forward")) {
+				if(forward != 0) {
+					System.out.println(UserName + " assigned a Role of " + UserRole);
+					
+					forward--;
+					isRoleAssigned = true;
+				}else {
+					System.out.println("This Role is No Longer Available");
+				}
+			}else {
+				System.out.println("Please Enter Valid Role");
+				sc.next();
+			}
+		}
+		
+	}
+
+}
